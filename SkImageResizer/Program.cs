bb77e803f08941948392402825350ad29cd731b7
shockliang <shockwater@gmail.com>
2020-09-07 19:06:43 +0800

    Adding async testing section.

--- SkImageResizer/Program.cs
+++ SkImageResizer/Program.cs
@@ -1,26 +1,31 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace SkImageResizer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
             var destinationPath = Path.Combine(Environment.CurrentDirectory, "output");
 
             var imageProcess = new SKImageProcess();
 
-            imageProcess.Clean(destinationPath);
-
             var sw = new Stopwatch();
+            imageProcess.Clean(destinationPath);
             sw.Start();
             imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
             sw.Stop();
-
-            Console.WriteLine($"花費時間: {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine($"同步花費時間: {sw.ElapsedMilliseconds} ms");
+            
+            imageProcess.Clean(destinationPath);
+            sw.Restart();
+            await imageProcess.ResizeImagesAsync(sourcePath, destinationPath, 2.0);
+            sw.Stop();
+            Console.WriteLine($"非同步花費時間: {sw.ElapsedMilliseconds} ms");
         }
     }
 }

0672caf488b9e72fd73ad01b5080e07b6d0300df
shockliang <shockwater@gmail.com>
2020-09-07 18:52:00 +0800

    Adding benchmark project.

---
+++ SkImageResizer.Benchmark/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace SkImageResizer.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // BenchmarkRunner.Run<Program>();   
+            // BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+            // BenchmarkRunner.Run(typeof(Program).Assembly);
+            BenchmarkRunner.Run<SkImageResizerBenchmark>();
+        }
+    }
+}

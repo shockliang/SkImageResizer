e341dc0095c9553513a1ac5260da7f52805a60c9
shockliang <shockwater@gmail.com>
2020-09-07 17:34:08 +0800

    Adding resize async way.

---
+++ SkImageResizerAsync/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SkImageResizerAsync
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
+            var destinationPath = Path.Combine(Environment.CurrentDirectory, "output");
+
+            var imageProcess = new SKImageProcess();
+
+            imageProcess.Clean(destinationPath);
+
+            var sw = new Stopwatch();
+            sw.Start();
+            await imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
+            sw.Stop();
+
+            Console.WriteLine($"花費時間: {sw.ElapsedMilliseconds} ms");
+            
+        }
+    }
+}
---
+++ SkImageResizerAsync/SkImageProcess.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using SkiaSharp;
+
+namespace SkImageResizerAsync
+{
+    public class SKImageProcess
+    {
+        /// <summary>
+        /// 進行圖片的縮放作業
+        /// </summary>
+        /// <param name="sourcePath">圖片來源目錄路徑</param>
+        /// <param name="destPath">產生圖片目的目錄路徑</param>
+        /// <param name="scale">縮放比例</param>
+        public async Task ResizeImages(string sourcePath, string destPath, double scale)
+        {
+            var allFiles = FindImages(sourcePath);
+            var tasks = new ConcurrentBag<Task>();
+            foreach (var filePath in allFiles)
+            {
+                var task = Task.Run(() =>
+                {
+                    var imgName = Path.GetFileNameWithoutExtension(filePath);
+                    Console.WriteLine($"tid: {Thread.CurrentThread.ManagedThreadId} running process image name:{imgName}");
+                    var bitmap = SKBitmap.Decode(filePath);
+                    var imgPhoto = SKImage.FromBitmap(bitmap);
+
+                    var sourceWidth = imgPhoto.Width;
+                    var sourceHeight = imgPhoto.Height;
+
+                    var destinationWidth = (int) (sourceWidth * scale);
+                    var destinationHeight = (int) (sourceHeight * scale);
+
+                    using var scaledBitmap = bitmap.Resize(
+                        new SKImageInfo(destinationWidth, destinationHeight),
+                        SKFilterQuality.High);
+                    using var scaledImage = SKImage.FromBitmap(scaledBitmap);
+                    using var data = scaledImage.Encode(SKEncodedImageFormat.Jpeg, 100);
+                    using var s = File.OpenWrite(Path.Combine(destPath, imgName + ".jpg"));
+                    data.SaveTo(s);
+                });
+
+                tasks.Add(task);
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        /// <summary>
+        /// 清空目的目錄下的所有檔案與目錄
+        /// </summary>
+        /// <param name="destPath">目錄路徑</param>
+        public void Clean(string destPath)
+        {
+            if (!Directory.Exists(destPath))
+            {
+                Directory.CreateDirectory(destPath);
+            }
+            else
+            {
+                var allImageFiles = Directory.GetFiles(destPath, "*", SearchOption.AllDirectories);
+
+                foreach (var item in allImageFiles)
+                {
+                    File.Delete(item);
+                }
+            }
+        }
+        
+        /// <summary>
+        /// 找出指定目錄下的圖片
+        /// </summary>
+        /// <param name="srcPath">圖片來源目錄路徑</param>
+        /// <returns></returns>
+        public List<string> FindImages(string srcPath)
+        {
+            List<string> files = new List<string>();
+            files.AddRange(Directory.GetFiles(srcPath, "*.png", SearchOption.AllDirectories));
+            files.AddRange(Directory.GetFiles(srcPath, "*.jpg", SearchOption.AllDirectories));
+            files.AddRange(Directory.GetFiles(srcPath, "*.jpeg", SearchOption.AllDirectories));
+            return files;
+        }
+    }
+}

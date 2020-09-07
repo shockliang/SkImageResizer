198df7bbf3dcf42c3ce5ba147ea61afe98ac8aee
shockliang <shockwater@gmail.com>
2020-09-07 16:08:32 +0800

    Initial project.

---
+++ SkImageResizer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace SkImageResizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
+            var destinationPath = Path.Combine(Environment.CurrentDirectory, "output");
+
+            var imageProcess = new SKImageProcess();
+
+            imageProcess.Clean(destinationPath);
+
+            var sw = new Stopwatch();
+            sw.Start();
+            imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
+            sw.Stop();
+
+            Console.WriteLine($"花費時間: {sw.ElapsedMilliseconds} ms");
+        }
+    }
+}
